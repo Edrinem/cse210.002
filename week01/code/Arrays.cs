939b27cbc1ca9577f01873e334be137e1c1acd31
Edrinem <edrinem0011@icloud.com>
2026-05-16 23:39:42 +0000

    arrays.cs

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -9,7 +9,7 @@ public static class Arrays
     public static double[] MultiplesOf(double number, int length)
     {
         // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
+        // Remember:always Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
